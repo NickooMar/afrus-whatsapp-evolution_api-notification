4fbc98c280192477263310200f637e23607afc38
NickooMar <nicoo.marsili@gmail.com>
2025-01-24 16:55:58 -0300

    fix(queue): remove unnecesary comments in protocols queue

--- internal/application/protocols/queue.go
+++ internal/application/protocols/queue.go
@@ -13,14 +13,4 @@ type Queue interface {
 	startConsuming(config queue.QueueConfig, msgs chan *interface{}) error
 	Publish(ctx context.Context, exchange, routingKey string, body []byte) error
 	Schedule(exchange, routingKey string, body []byte, delay int) error
-
-	// Dial() error
-	// Setup() error
-	// Close() error
-	// Consume(queue string) error
-	// DeclareQueue(queue string) error
-	// BindQueue(exchange, routingKey, queue string) error
-	// Publish(ctx context.Context, exchange, routingKey string, body []byte) error
-	// DeclareExchange(exchange, kind string, delayed bool) error
-	// Schedule(exchange, routingKey string, body []byte, delay int) error
 }
